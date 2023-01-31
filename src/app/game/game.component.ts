e125065b438e4c370d7d1d70f9554b9e0484ad02
Dennis Ludmann <dennisludmann@gmail.com>
2023-01-31 20:51:11 +0100

    WIP top card showing

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,22 +6,35 @@ import { Game } from 'src/models/game';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  // game: Game;
+  currentCard: any = '';
+  game: Game;
 
-  constructor() { }
+
+  constructor() {
+    this.game = new Game();
+  }
 
   ngOnInit(): void {
     this.newGame();
   }
 
   newGame() {
-    // this.game = new Game();
+    this.game = new Game();
+    console.log(this.game);
   }
 
-  pickCard(){
-    this.pickCardAnimation = true;
+  pickCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.game.playedCards.push(this.currentCard);
+      console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
-
 }
